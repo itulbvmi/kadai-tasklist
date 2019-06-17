f1f0bfc127c6d921fdbd6c978e40151330d841c1
mi <itulbv.mi@gmail.com>
2019-06-17 08:22:13 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
   # get 'toppages/index'
   # root to: 'tasks#index'
   resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
 end
